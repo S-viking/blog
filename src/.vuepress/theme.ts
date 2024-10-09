913ecd3740017e5741792b05a7a2634995c834ad
zhengcr <zhengcr@eastcom-sw.com>
2024-10-09 11:38:47 +0800

    feat：修改

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -48,9 +48,9 @@ export default hopeTheme({
   },
 
   // 多语言配置
-  metaLocales: {
-    editLink: "在 GitHub 上编辑此页",
-  },
+  // metaLocales: {
+  //   editLink: "在 GitHub 上编辑此页",
+  // },
 
   // 如果想要实时查看任何改变，启用它。注: 这对更新性能有很大负面影响
   // hotReload: true,
